15d3debccbf738c2a227207fef8c9af1e6e71861
Engeldark <morenomoralesluisangel@hotmail.com>
2022-11-13 20:03:49 -0600

    creado el proyecto, asi como archivos ui

---
+++ ScriptEsc/main.py
@@ -0,0 +1,9 @@
+from PySide6.QtWidgets import QApplication
+import sys
+
+if __name__ == '__main__':
+    app: object = QApplication(sys.argv)
+    app_init: object = p()
+    app_init.show()
+    sys.exit(app.exec())
+    
